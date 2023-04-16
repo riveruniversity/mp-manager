8867e8ff626f966028a5adc144a5c0c4fc06ad8a
River University <rutech@revival.com>
2023-04-16 06:40:06 -0400

    0.0.3
    crop faces: https://stackoverflow.com/questions/67543997/is-there-any-way-to-auto-crop-the-face-after-doing-face-detection-with-face-api

--- src/api.ts
+++ src/api.ts
@@ -1,88 +1,134 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig, ResponseType } from 'axios'
 import { AxiosResponse } from 'axios'
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const g = {
+  member: 504,
+  staff: 490,
+  contractor: 491,
+  intern: 363,
+  waiver: 500
+}
+
 interface Parameter {
-    method: 'get' | 'post' | 'put';
-    select?: string;
-    filter?: string;
-    top?: string;
-    data?: any;
+  method: 'get' | 'post' | 'put';
+  select?: string;
+  filter?: string;
+  top?: string;
+  data?: any;
+  scope?: string;
+  responseType?: ResponseType;
 }
 
 export function getBlankCardIds() {
 
-    const table = `Group_Participants`
-    const select = `$select=Participant_ID_Table_Contact_ID_Table.Contact_ID, Participant_ID_Table_Contact_ID_Table.ID_Card, Participant_ID_Table_Contact_ID_Table.Display_Name`
-    const filter = `&$filter=Group_ID=${process.env.GRP_WVR} AND Participant_ID_Table_Contact_ID_Table.ID_Card IS null`
-    const top = `&$top=5000`
+  const table = `Group_Participants`
+  const select = `$select=Participant_ID_Table_Contact_ID_Table.Contact_ID, Participant_ID_Table_Contact_ID_Table.ID_Card, Participant_ID_Table_Contact_ID_Table.Display_Name`
+  const filter = `&$filter=Group_ID=${g.waiver} AND Participant_ID_Table_Contact_ID_Table.ID_Card IS null AND NOT Participant_ID_Table_Contact_ID_Table.Contact_ID<20`
+  const top = `&$top=5000`
 
-    return request(table, {method: 'get', select, filter, top})
+  return request(table, { method: 'get', select, filter, top })
+}
+//: --------------------------------------------------------
+
+export function getAllRiverMembers() {
+
+  const table = `Group_Participants`
+  const select = `$select=Group_Participants.Group_ID, Participant_ID_Table_Contact_ID_Table.Contact_ID, Participant_ID_Table_Contact_ID_Table.ID_Card, Participant_ID_Table_Contact_ID_Table.First_Name, Participant_ID_Table_Contact_ID_Table.Last_Name, Participant_ID_Table_Contact_ID_Table.Mobile_Phone, Participant_ID_Table_Contact_ID_Table.dp_fileUniqueId Image`
+  const filter = `&$filter=Group_Participants.Group_ID IN (${g.waiver},${g.member},${g.staff},${g.intern},${g.contractor}) AND NOT Participant_ID_Table_Contact_ID_Table.Contact_ID<20 AND Participant_ID_Table.Participant_Engagement_ID=1` // AND Not Participant_ID_Table_Contact_ID_Table.ID_Card Like 'M-%'   ||  AND Participant_ID_Table_Contact_ID_Table.Contact_ID=126634
+  const top = `&$top=50000`
+
+  return request(table, { method: 'get', select, filter, top })
 }
 //: --------------------------------------------------------
 
 
 export function getRiverMembers() {
 
-    const table = `Group_Participants`
-    const select = `$select=Participant_ID_Table_Contact_ID_Table.Contact_ID, Participant_ID_Table_Contact_ID_Table.ID_Card, Participant_ID_Table_Contact_ID_Table.Display_Name`
-    const filter = `&$filter=Group_ID=${process.env.GRP_WVR} AND Group_ID=${process.env.GRP_MMBR} AND Group_Participants.Group_ID NOT IN (${process.env.GRP_STFF},${process.env.GRP_INTR},${process.env.GRP_CNTR})`
-    const top = `&$top=5000`
+  const table = `Group_Participants`
+  const select = `$select=Group_Participants.Group_ID, Group_Participants.Group_Participant_ID, Participant_ID_Table_Contact_ID_Table.Contact_ID, Participant_ID_Table_Contact_ID_Table.ID_Card, Participant_ID_Table_Contact_ID_Table.Display_Name`
+  const filter = `&$filter=Group_Participants.Group_ID IN (${g.waiver},${g.member}) AND NOT Participant_ID_Table_Contact_ID_Table.Contact_ID<20` // AND Participant_ID_Table_Contact_ID_Table.Contact_ID=126634
+  const top = `&$top=50000`
 
-    console.log(filter)
+  return request(table, { method: 'get', select, filter, top })
+}
+//: --------------------------------------------------------
 
-    return request(table, {method: 'get', select, filter, top})
+
+export function getRiverStaff() {
+
+  const table = `Group_Participants`
+  const select = `$select=Group_Participants.Group_ID, Group_Participants.Group_Participant_ID, Participant_ID_Table_Contact_ID_Table.Contact_ID, Participant_ID_Table_Contact_ID_Table.ID_Card, Participant_ID_Table_Contact_ID_Table.Display_Name`
+  const filter = `&$filter=Group_Participants.Participant_ID = Participant_ID_Table_Contact_ID_Table.Participant_Record AND Group_Participants.Group_ID IN (${g.staff},${g.intern},${g.contractor}) AND NOT Participant_ID_Table_Contact_ID_Table.Contact_ID<20`
+  const top = `&$top=1000`
+
+  return request(table, { method: 'get', select, filter, top })
 }
 //: --------------------------------------------------------
 
 
 export function getFormResponses(eid: number) {
 
-    const table = `Form_Responses`
-    const select = `$select=Form_Responses.Contact_ID, Form_Responses.Phone_Number, Contact_ID_Table.ID_Card, Contact_ID_Table.First_Name, Contact_ID_Table.Last_Name`
-    const filter = `&$filter=Event_ID=${eid} AND Phone_Number is not null AND Contact_ID_Table.ID_Card is null` //AND Contact_ID_Table.ID_Card is not null AND Contact_ID_Table.ID_Card NOT Like 'C%'
-    const top = `&$top=5000`
+  const table = `Form_Responses`
+  const select = `$select=Form_Responses.Contact_ID, Form_Responses.Phone_Number, Contact_ID_Table.ID_Card, Contact_ID_Table.First_Name, Contact_ID_Table.Last_Name`
+  const filter = `&$filter=Event_ID=${eid} AND Phone_Number is not null AND Contact_ID_Table.ID_Card is null` //AND Contact_ID_Table.ID_Card is not null AND Contact_ID_Table.ID_Card NOT Like 'C%'
+  const top = `&$top=5000`
 
-    return request(table, {method: 'get', select, filter, top})
+  return request(table, { method: 'get', select, filter, top })
 }
 //: --------------------------------------------------------
 
 
 export function putCardId(Contact_ID: number, ID_Card: string) {
 
-    const table = `Contacts`
-    
-    const data = [{Contact_ID, ID_Card}];
+  const table = `Contacts`
 
-    return request(table, {method: 'put', data})
+  const data = [{ Contact_ID, ID_Card }];
+
+  return request(table, { method: 'put', data })
+}
+//: --------------------------------------------------------
+
+
+export function getImage(fileId: string) {
+
+  const table = `Files`
+
+  return request(table, { method: 'get', scope: 'files', select: fileId, responseType: 'arraybuffer' })
 }
 //: --------------------------------------------------------
 
 
 async function request(table: string, param: Parameter) {
 
-    const baseUrl = `https://mp.revival.com/ministryplatformapi/tables/${table}?`
-    
-    var config = {
-        method: param.method,
-        url: baseUrl + param.select + param.filter + param.top,
-        headers: { 
-            'Authorization': 'Bearer ' + process.env.TOKEN,
-            'Content-Type': 'application/json'
-        },
-        data: param.data
-    };
-    
-    try {
-        const response: AxiosResponse = await axios(config);
-        return response.data;
-    } catch (error: any) {
-        console.log(error.response.data);
-        //console.log('Error when requesting ', param.data);
-        console.log(error.response.status, error.response.statusText);
-        
-    }
+  const baseUrl = `https://mp.revival.com/ministryplatformapi`
+  param.scope = param.scope ? `/${param.scope}/` : `/tables/${table}?`
+
+  var config: AxiosRequestConfig = {
+    method: param.method,
+    url: baseUrl + param.scope + (param.select || '') + (param.filter || '') + (param.top || ''),
+    headers: {
+      'Authorization': 'Bearer ' + process.env.TOKEN,
+      'Content-Type': 'application/json'
+    },
+    data: param.data,
+  };
+
+  if (param.responseType) {
+    config.responseType = param.responseType
+    // config.responseEncoding = "base64"
+  }
+
+  try {
+    const response: AxiosResponse = await axios(config);
+    return response.data;
+  } catch (error: any) {
+    console.log(error.cause);
+    console.log('Error when requesting ', config.url);
+    console.log(error.response?.status, error.response?.statusText);
+
+  }
 }
 //: --------------------------------------------------------
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,76 @@
-import { getBlankCardIds, getFormResponses, getRiverMembers, putCardId } from './api'
+import { getBlankCardIds, getFormResponses, getRiverMembers, getRiverStaff, getAllRiverMembers, putCardId, getImage } from './api'
+import * as fs from 'fs'
 
 interface Contact {
-    Contact_ID: number;
-    ID_Card: null | string;
-    Display_Name: string;
-    First_Name: string;
-    Last_Name: string;
+  Contact_ID: number;
+  Group_ID: number;
+  ID_Card: null | string;
+  Display_Name: string;
+  First_Name: string;
+  Last_Name: string;
+  Mobile_Phone: string;
+  Image: string | null;
+  Img: Buffer;
 }
+
+const staffGroupIds: number[] = [490, 491, 363]
 
 async function populateCardId() {
-    const eventId = 68302
-    // const contacts = await getBlankCardIds()
-    // const contacts = await getFormResponses(eventId)
-    const contacts = await getRiverMembers();
-    
-    if(!contacts) return 
+  const eventId = 68302
+  // const contacts = await getBlankCardIds()
+  // const contacts = await getFormResponses(eventId)
+  // const contacts = await getRiverMembers();
+  const contacts = await getAllRiverMembers();
+  // const riverStaff = await getRiverStaff();
 
-    console.log(`${contacts.length} contacts`, contacts)
-    console.log(`${contacts.length} contacts`)
-    
-    return
-    contacts.forEach(async (contact: Contact) => {
-        //console.log(contact);
-        const card = await putCardId(contact.Contact_ID, "C"+contact.Contact_ID)
-        console.log('success', card[0].Contact_ID, card[0].First_Name);
-    });
+  if (!contacts) return
 
-    // *.rmiwebservices.com
+  console.log(`${contacts.length} contacts`)
+
+  const groupedContacts = groupBy(contacts, 'Contact_ID')
+  const noStaffMembers: Contact[][] = groupedContacts.filter((contacts: Contact[]) => !contacts.find((contact: Contact) => staffGroupIds.includes(contact.Group_ID)) && contacts.find((contact: Contact) => contact.Group_ID === 500) && contacts.find((contact: Contact) => contact.Group_ID === 504))
+  const cleanMembersArr: Contact[] = noStaffMembers.map((contacts: Contact[]) => contacts[0])
+  const membersWithPic = cleanMembersArr.filter((contact: Contact) => contact.Image)
+
+  console.log(`cleanMembersArr: ${cleanMembersArr.length} contacts`)
+  console.log(`membersWithPic: ${membersWithPic.length} contacts`)
+  membersWithPic.forEach((contact: Contact, i: number) => getImage(String(contact.Image)).then((blob: Buffer) => fs.writeFileSync('./src/img/'+ contact.Contact_ID +'.jpeg', blob)))
+  // fs.writeFileSync('./src/img/'+ contact.Contact_ID +'.jpeg', blob)
+  // fs.writeFileSync('./src/cleanMembers.json', JSON.stringify(cleanMembersArr, null, '\t'));
+  fs.writeFileSync('./src/membersWithPic.json', JSON.stringify(membersWithPic, null, '\t'));
+
+  // updateCardId(cleanMembersArr);
+}
+
+populateCardId()
+
+
+
+function updateCardId(arr: Contact[]) {
+  arr.forEach((contact: Contact) => {
+    putCardId(contact.Contact_ID, "M-" + contact.Contact_ID).then((card) => console.log('success', card[0].Contact_ID, card[0].First_Name))
+  })
 }
 
 
-populateCardId()
+function groupBy(array: any[], key: string) {
+  const participants: any[] = [];
+
+  var outObject = array.reduce(function (a, e) {
+    // GROUP BY estimated key (estKey), well, may be a just plain key
+    // a -- Accumulator result object
+    // e -- sequentially checked Element, the Element that is tested just at this iteration
+
+    // new grouping name may be calculated, but must be based on real value of real field
+    let estKey = (e[key]);
+
+    (a[estKey] ? a[estKey] : (a[estKey] = null || [])).push(e);
+    return a;
+  }, {});
+
+  for (var key in outObject) {
+    participants.push(outObject[key]);
+  }
+
+  return participants;
+}
