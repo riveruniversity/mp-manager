9f656bdb3a966e590ef5aec7b1a4b80fe53b47e7
Wilhelm <wmauch@revival.com>
2025-11-05 09:15:35 -0500

    Update api-test.ts

--- src/tasks/api-test.ts
+++ src/tasks/api-test.ts
@@ -1,6 +1,7 @@
-import { CreateFormResponseAnswerParams } from "mp-api";
+import { Contact } from "mp-api";
 import { mp } from "../services/mp/api";
-import { findContact } from "../services/mp/find";
+import { sleep } from "../utils";
+// import { findContact } from "../services/mp/find";
 
 function getEvent() {
   mp.getEvent(70551)
@@ -21,7 +22,7 @@ function getContacts() {
 }
 
 function createFormResponse(formID: number = 49) {
-  mp.createFormResponse({ formID, responseDate: new Date().toISOString().split('T')[0] + 'T00:00:00'  })
+  mp.createFormResponse({ formID, responseDate: new Date().toISOString().split('T')[0] + 'T00:00:00' })
     .then(res => res && 'error' in res ? console.log(res.error) : console.log(res))
     .catch(err => console.log(err));
 }
@@ -37,7 +38,13 @@ function updateContacts() {
     .then(res => res && 'error' in res ? console.log(res.error) : console.log(res))
     .catch(err => console.log(err));
 }
- 
+
+function updatePcoId() {
+  mp.updateContacts([{ contactID: 119178, planningCenterID: 40790355 }])
+    .then(res => res && 'error' in res ? console.log(res.error) : console.log(res))
+    .catch(err => console.log(err));
+}
+
 function getContactEmailAddresses() {
   mp.getContactEmailAddresses({ filter: 'Contact_ID=126634' })
     .then(res => res && 'error' in res ? console.log(res.error) : console.log(res))
@@ -62,8 +69,37 @@ function getContactDetails() {
     .catch(err => console.log(err));
 }
 
-findContact({ firstName: 'Aaliyah', lastName: 'Montoya', emailAddress: 'graceontopofgrace123@gmail.com' })
-  .then(contact => console.log(contact))
-  .catch(err => console.log(err));
+async function createHouseholds() {
+  const contacts = (await mp.getEventParticipants({
+    select: `Event_Participants.Notes,Event_Participants.Group_Participant_ID, Event_ID_Table.Event_Start_Date, Participant_ID_Table_Contact_ID_Table.*`,
+    filter: `Event_Participants.Event_ID=71127 AND Participant_ID_Table_Contact_ID_Table.Household_ID Is Null`
+  }) as unknown as Contact[]);
 
-getContactDetails();
+  if (!('error' in contacts)) {
+    console.log(contacts.length);
+let i = 1;
+    for (let c of contacts) {
+      let household = await mp.createHousehold({ householdName: c.lastName! });
+      // .then(res => res && 'error' in res ? console.log(res.error) : console.log(res))
+      // .catch(err => console.log(err));
+      
+
+      if (!('error' in household)) {
+        mp.updateContacts([{ contactID: c.contactID, householdID: household.householdID }]);
+        console.log(++i)
+      } else {
+        console.warn(household);
+      }
+    };
+  }
+  else {
+    console.warn(contacts);
+  }
+
+}
+
+// findContact({ firstName: 'Aaliyah', lastName: 'Montoya', emailAddress: 'graceontopofgrace123@gmail.com' })
+//   .then(contact => console.log(contact))
+//   .catch(err => console.log(err));
+
+createHouseholds();
